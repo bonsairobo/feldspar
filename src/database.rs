ed4c34e1ca481342f992c19aad00fafc53bdd071
Duncan <bonsairobo@gmail.com>
2021-06-21 12:30:29 -0700

    merge with upstream building-blocks

--- src/database.rs
+++ src/database.rs
@@ -1,9 +1,22 @@
 use crate::VoxelType;
 
-use building_blocks::storage::{ChunkDb3, FastArrayCompressionNx2, Lz4, Sd8};
+use building_blocks::storage::{
+    sled, ChunkDb3, FastArrayCompressionNx2, FromBytesCompression, Lz4, Sd8,
+};
 
 pub struct VoxelWorldDb {
     chunks: SdfChunkDb,
 }
 
 pub type SdfChunkDb = ChunkDb3<FastArrayCompressionNx2<[i32; 3], Lz4, VoxelType, Sd8>>;
+
+impl VoxelWorldDb {
+    pub fn new(tree: sled::Tree) -> Self {
+        Self {
+            chunks: ChunkDb3::new(
+                tree,
+                FastArrayCompressionNx2::from_bytes_compression(Lz4 { level: 10 }),
+            ),
+        }
+    }
+}
