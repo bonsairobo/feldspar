d18e0101acaaf98e6ed0a3fed996500e95b00ea7
Duncan <bonsairobo@gmail.com>
2021-06-22 01:45:49 -0700

    add method VoxelWorldDb::load_chunks_into_map

--- src/database.rs
+++ src/database.rs
@@ -1,7 +1,8 @@
-use crate::VoxelType;
+use crate::{SdfVoxelMap, VoxelType};
 
-use building_blocks::storage::{
-    sled, ChunkDb3, FastArrayCompressionNx2, FromBytesCompression, Lz4, Sd8,
+use building_blocks::{
+    core::prelude::*,
+    storage::{sled, ChunkDb3, FastArrayCompressionNx2, FromBytesCompression, Lz4, Sd8},
 };
 
 pub struct VoxelWorldDb {
@@ -18,5 +19,21 @@ impl VoxelWorldDb {
                 FastArrayCompressionNx2::from_bytes_compression(Lz4 { level: 10 }),
             ),
         }
+    }
+
+    pub async fn load_chunks_into_map(
+        &self,
+        lod: u8,
+        extent: Extent3i,
+        map: &mut SdfVoxelMap,
+    ) -> sled::Result<()> {
+        // Heuristic: We want the orthant edge length to be about 1/2 the extent's smallest dimension.
+        let orthant_edge_length = extent.shape.min_component() >> 1;
+        let orthant_exponent = orthant_edge_length.trailing_zeros() as i32;
+        self.chunks
+            .read_orthants_covering_extent(lod, orthant_exponent, extent, |key, chunk| {
+                map.voxels.write_chunk(key, chunk)
+            })
+            .await
     }
 }
